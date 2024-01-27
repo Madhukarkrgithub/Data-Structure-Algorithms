1886578829c3b406f6edeed167caefe75f54b9e3
Madhukar kumar <136974712+Madhukarkrgithub@users.noreply.github.com>
2024-01-27 20:52:37 +0530

    Create Lower bound in logn.java

---
+++ Unit - 3/Binary Search/Lower bound in logn.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Main{
+    public static int lowerBond(int []arr, int k){
+        int low = 0;
+        int high= arr.length-1;
+        int ans = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==k){
+                ans = mid;
+                high = mid -1;
+                
+            }else if(k>arr[mid]){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int []arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int ans = lowerBond(arr,k);
+        System.out.println(ans);
+    }
+}
