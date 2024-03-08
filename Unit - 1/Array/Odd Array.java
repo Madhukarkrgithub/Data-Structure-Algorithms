d4d8844c9e49890d5e708caba1988cfc5ed432ea
Madhukar kumar <136974712+Madhukarkrgithub@users.noreply.github.com>
2024-03-08 23:49:18 +0530

    Create Odd Array.java

---
+++ Unit - 1/Array/Odd Array.java
@@ -0,0 +1,7 @@
+  public static void oddArray(int n, int arr[]){
+         for(int i=0;i<n;i++){
+             if(arr[i]%2 !=0){
+                 System.out.println(arr[i]);
+             }
+         }
+  }
