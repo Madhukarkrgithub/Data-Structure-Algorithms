a57764c65a3b42bba2b22c826b050ab79bbc3091
Madhukar kumar <136974712+Madhukarkrgithub@users.noreply.github.com>
2024-03-10 12:51:26 +0530

    Create Pattern of N.java

---
+++ Unit - 1/Pattern/Pattern of N.java
@@ -0,0 +1,10 @@
+public static void pattern(int N){
+      int count = 1;
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            System.out.print(count+" ");
+            count++;
+        }
+        System.out.println();
+    }
+  }
