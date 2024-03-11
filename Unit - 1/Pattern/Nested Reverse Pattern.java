c30c432a58716f0ad44ce8ed14dbbf914f98a5dc
Madhukar kumar <136974712+Madhukarkrgithub@users.noreply.github.com>
2024-03-11 21:55:57 +0530

    Create Nested Reverse Pattern.java

---
+++ Unit - 1/Pattern/Nested Reverse Pattern.java
@@ -0,0 +1,8 @@
+  public static void solve(int num){
+      for(int i=1;i<=num;i++){
+          for(int j=num;j>0;j--){
+              System.out.print(j+" ");
+          }
+          System.out.println();
+      }
+  }
